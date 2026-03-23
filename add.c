0bc95cf626afe7b497f6ed7688450e271c9b3bcf
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-03-23 19:42:48 +0530

    fist code

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("enter a first num ");
+    scanf("%d",&num1);
+    printf("enter a second num ");
+    scanf("%d",&num2);
+
+    sum= num1+num2 ;
+    printf("the sum of %d and %d is %d \n ", num1,num2,sum);
+
+    return 0;
+}
