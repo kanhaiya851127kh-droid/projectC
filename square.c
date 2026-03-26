45aa893b38f1acaadbc3387d637aac5bbcda7f8f
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-03-26 22:15:27 +0530

    fist code

---
+++ square.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main(){
+    int num ,square ;
+    printf("enter a first num ");
+    square = num *num ;
+
+    printf("the square of %d is : ", num,square);
+
+    return 0;
+}
--- subtraction.c
+++ subtraction.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+'''#include<stdio.h>
 int main(){
     int num1,num2,difference ;
     printf("enter a first num ");
@@ -8,6 +8,22 @@ int main(){
 
     difference = num1-num2 ;
     printf("the difference of %d and %d is %d \n ", num1,num2,difference);
+
+    return 0;
+}'''
+
+
+
+#include<stdio.h>
+int main() {
+    int num1,num2,product ;
+    printf("enter a first num ");
+    scanf("%d",&num1);
+    printf("enter a second num ");
+    scanf("%d",&num2);
+
+    product = num1*num2 ;
+    printf("the product of %d and %d is %d \n ", num1,num2,product);
 
     return 0;
 }
