748719fe32ce7bbecc5e76d211954c03bd4fe43d
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-03-27 22:06:05 +0530

    fist code

---
+++ que.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
