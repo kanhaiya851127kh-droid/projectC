26d1d77a6004ac650de8a321cc402968ee4daf46
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-04-11 23:04:26 +0530

    fist code

--- whileo.c
+++ whileo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
     int i = 1;
@@ -7,6 +7,21 @@ int main() {
         printf("%d\n", i);
         i++;
     } while (i <= 5);
+
+    return 0;
+}*/
+
+
+
+#include <stdio.h>
+
+int main() {
+    int i = 5;
+
+    do {
+        printf("%d\n", i);
+        i--;
+    } while (i >= 1);
 
     return 0;
 }
