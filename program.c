540f1aa36d3421520b2e326624ca886eda6563ea
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-06-01 23:36:47 +0530

    fist code

--- program.c
+++ program.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-void display (int arr[], int n) {
+void display (int arr[], int n) { 
     int i;
     for (i = 0; i < n; i++)  // that a loop to display the array elements
-        printf("%d", arr[i]);
+        printf("%d", arr[i]); // output the array elements
 
     }
 
