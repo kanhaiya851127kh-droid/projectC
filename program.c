42f4bd38a02468f155173c606749c83c89ff0a6f
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-06-03 20:57:27 +0530

    fist code

--- program.c
+++ program.c
@@ -2,12 +2,12 @@
 
 void display (int arr[], int n) { 
     int i;
-    for (i = 0; i < n; i++)  // that a loop to display the array elements
-        printf("%d", arr[i]); // output the array elements
+    for (i = 0; i < n; i++)  
+        printf("%d", arr[i]); 
 
     }
 
-int  main (){  // integar type main function
+int  main (){  
     int a[5] = {1,2,3,4,5};
     display(a,5);
     return 0;     
