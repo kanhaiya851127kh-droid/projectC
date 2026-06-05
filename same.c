aea9b8baf6c53b95d166a8c07ceddb1b1865dcfe
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-06-05 18:49:31 +0530

    fist code

---
+++ same.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void display (int arr[], int n) { 
+    int i;
+    for (i = 0; i < n; i++)  
+        printf("%d", arr[i]); 
+
+    }
+
+int  main (){  
+    int a[5] = {1,2,3,4,5};
+    display(a,5); 
+    return 0;     
+}
