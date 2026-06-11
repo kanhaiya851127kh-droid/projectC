57250a332a4cf2a774d0b12fab1cf7eab368fc58
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-06-11 21:30:48 +0530

    fist code

--- same.c
+++ same.c
@@ -4,11 +4,11 @@ int main () {
     heart*you = trust();
     you->value = 100;
     you->care = true;
-    you->loyalty = infinte;
+    you->loyalty = infinte;//trest ishu in life for you.
 
     commit(you);
 
-    heart* result = returnformyou();
+    heart* result = returnformyou();//you will return to me or not..
     if(result == NULL) {
         printf("she returned NULL .\n");
         heart.break();
