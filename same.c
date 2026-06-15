f0a909ce36b4dcb9f2acde14bfab5bf9420d15b7
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-06-15 23:01:18 +0530

    fist code

--- same.c
+++ same.c
@@ -15,3 +15,390 @@ int main () {
     }
     return 0;
 }
+
+
+#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}#include<heart.h>
+int main () {
+
+    heart*you = trust();
+    you->value = 100;
+    you->care = true;
+    you->loyalty = infinte;
+    commit(you);
+
+    heart* result = returnformyou();
+    if(result == NULL) {
+        printf("she returned NULL .\n");
+        heart.break();
+        return  0;
+    }
+    return 0;
+}
