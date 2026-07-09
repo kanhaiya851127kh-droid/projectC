80267062904e4907c8dbc7e080587e3e4eb24b95
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-07-09 22:30:40 +0530

    fist code

---
+++ calloc.c
@@ -0,0 +1,201 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+
+	int *arr, size;
+	printf("Enter the no of elements to be entered:\n");
+	scanf("%d", &size);
+
+	arr = (int*) calloc(size, sizeof(int));
+
+	printf("Enter %d numbers:\n", size);
+	for(int i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Square of entered %d numbers are:\n", size);
+	for(int i = 0; i < size; i++){
+		printf("%d, ", ((int) pow(arr[i], 2)));
+	}
+
+	return 0;
+}
