184250ef28803522b38f986769166c6500696233
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-07-26 21:59:04 +0530

    fist code

--- que.c
+++ que.c
@@ -9,3 +9,65 @@ int main(){
 
     return 0;
 }
+
+
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int num ,queb ;
+    printf("enter a first num ");
+    queb = num *num*num ;
+
+    printf("the queb of %d is : ", num,queb);
+
+    return 0;
+}
