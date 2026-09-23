11094adab38805ab24077a8ebde57e8a01a967d2
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-09-23 22:22:38 +0530

    fist code

--- copy.c
+++ copy.c
@@ -14,47 +14,6 @@
                     }
                 }
             }
-
-            else if (choice == 3) {
-
-                printf("\nFollowing:\n");
-
-                if (currentUser->followingCount == 0) {
-                    printf("No following users.\n");
-                }
-                else {
-                    for (int i = 0;
-                         i < currentUser->followingCount;
-                         i++) {
-
-                        printf("%s\n",
-                               currentUser->following[i]->username);
-                    }
-                }
-            }
-
-            else if (choice == 4) {
-
-                scanf("%20s", username);
-
-                struct User *otherUser =
-                    findUser(head, username);
-
-                if (unfollowUser(currentUser, otherUser))
-                    printf("Successfully unfollowed %s.\n",
-                           username);
-                else
-                    printf("Cannot unfollow %s.\n", username);
-            }
-
-            else if (choice == 5) {
-
-                scanf("%20s", username);
-
-                struct User *otherUser =
-                    findUser(head, username);
-
-                if (removeFollower(currentUser, otherUser))
                     printf("%s was removed from your followers.\n", username);
                 else
                     printf("%s is not your follower.\n", username);
